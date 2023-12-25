e36cc06e6e7fc50361d3bfd3ab7ab15d409e42dd
Tina <tinaachenn14@gmail.com>
2023-12-25 01:49:00 -0700

    password function

---
+++ scripts/monse.js
@@ -0,0 +1,17 @@
+const toggleModal = (event) => {
+    event.preventDefault();
+    const passwordInput = document.getElementById("modalPasswordInput");
+    console.log(passwordInput.value);
+    if (passwordInput.value === "buttholedog") {
+        const modal = document.getElementById("password-modal");
+        modal.style.display = "none";
+        modal.classList.remove('error');
+    }else {
+        passwordInput.classList.add('error');
+        passwordInput.value = "";
+    }
+}
+
+const form = document.getElementById("passwordForm");
+form.addEventListener("submit", toggleModal);
+
